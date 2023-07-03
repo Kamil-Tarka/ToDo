dfa9e3e738f459b76728c9ed37836af742b4b337
Kamil Tarka <kamiltarka5@gmail.com>
2023-07-03 15:51:37 +0200

    Add project files.

---
+++ ToDoCore/ViewModels/Pages/WorkTaskPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ToDoCore.ViewModels.Controls;
+
+namespace ToDoCore.ViewModels.Pages
+{
+    public class WorkTaskPageViewModel
+    {
+        public List<WorkTaskViewModel> WorkTaskList { get; set; }
+        public string NewWorkTaskTitle { get; set; }
+        public string NewWorkTaskDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        private void AddNewTask()
+        {
+        }
+    }
+}
